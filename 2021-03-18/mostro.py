8abb7fc2d1260d95f2eb79c6da5b35bdd75f0ed9
unknown <enricovirgilio.rossi@gmail.com>
2021-03-24 21:16:33 +0100

    modifico mostro.py

--- 2021-03-18/mostro.py
+++ 2021-03-18/mostro.py
@@ -7,17 +7,55 @@ class Entity:
         self.damage = damage
         self.field = field
         self.field.entities_list.append(self)
+  
+    def check_collision(self):
+        for e in self.field.entities_list:
+            if self == e:
+                continue
+            elif self.x == e.x and self.y == e.y:
+                return True
 
-    def move(self, direction):
-        if direction == "up" and self.y > 0:
+    def move(self):
+        if keyboard.is_pressed("w") and self.y > 0:
+            while keyboard.is_pressed("w"):
+                continue
             self.y -= 1
-        elif direction == "right" and self.x < self.field.w - 1:
+            if self.check_collision():
+                self.y += 1
+            else:  
+                print()
+                self.field.draw()
+            
+        elif keyboard.is_pressed("d") and self.x < self.field.w -1:
+            while keyboard.is_pressed("d"):
+                continue
             self.x += 1
-        elif direction == "down" and self.y < self.field.h - 1:
+            if self.check_collision():
+                self.x -= 1
+            else:  
+                print()
+                self.field.draw()
+            
+        elif keyboard.is_pressed("s") and self.y < self.field.h -1:
+            while keyboard.is_pressed("s"):
+                continue
             self.y += 1
-        elif direction == "left" and self.x > 0:
+            if self.check_collision():
+                self.y -= 1
+            else:  
+                print()
+                self.field.draw()
+            
+        elif keyboard.is_pressed("a") and self.x > 0:
+            while keyboard.is_pressed("a"):
+                continue
             self.x -= 1
-
+            if self.check_collision():
+                self.x += 1
+            else:  
+                print()
+                self.field.draw()
+    
 class Monster(Entity):
     def __init__(self, hp, x, y, damage, field):
         super().__init__(hp, x, y, damage, field)
@@ -25,6 +63,13 @@ class Monster(Entity):
     def attack(self, monster2):
         monster2.hp -= self.damage
         
+    def change_monster(self):
+        if keyboard.is_pressed("space"):
+            while keyboard.is_pressed("space"):
+                continue
+            return True
+        
+
 class Field():
     def __init__(self, h, w):
         self.h = h
@@ -41,6 +86,10 @@ class Field():
                 else:
                     print("[ ]", end = "")
             print()
+            
+def close_program():
+    if keyboard.is_pressed("e"):
+        quit()
     
 field = Field(10, 10)
 monster1 = Monster(10, 5, 5, 20, field)
@@ -50,51 +99,8 @@ field.draw()
 while True:
     for e in field.entities_list:
         while True:
-            if keyboard.is_pressed("w"):
-                e.move("up")
-                print()
-                field.draw()
-                while keyboard.is_pressed("w"):
-                    continue
-                
-            elif keyboard.is_pressed("d"):
-                e.move("right")
-                print()
-                field.draw()
-                while keyboard.is_pressed("d"):
-                    continue
-            
-            elif keyboard.is_pressed("s"):
-                e.move("down")
-                print()
-                field.draw()
-                while keyboard.is_pressed("s"):
-                    continue
-                
-            elif keyboard.is_pressed("a"):
-                e.move("left")
-                print()
-                field.draw()
-                while keyboard.is_pressed("a"):
-                    continue
-                
-            elif keyboard.is_pressed("space"):
-                if e == field.entities_list[-1]:
-                    break
-                else:
-                    while keyboard.is_pressed("space"):
-                        continue
-                    break
-            
-            elif keyboard.is_pressed("e"):
+            e.move()
+            if e.change_monster():
                 break
-        if keyboard.is_pressed("space"):
-            while keyboard.is_pressed("space"):
-                continue
-            break
-            
-        if keyboard.is_pressed("e"):
-            break
-        
-    if keyboard.is_pressed("e"):
-        break
+            if close_program():
+                break
